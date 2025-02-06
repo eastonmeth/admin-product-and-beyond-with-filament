23ccaa21cd3339115461b567137a9b484d76d966
Easton Meth <easton.meth@gmail.com>
2025-02-06 14:33:04 -0500

    Add Posts and Comments to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $users = User::factory(10)->create();
+
+        foreach ($users as $user) {
+            Post::factory(5)
+                ->has(Comment::factory(
+                    5,
+                    fn (): array => [
+                        'user_id' => $users->where('id', '!=', $user->id)->random()->id,
+                    ]
+                ))
+                ->create([
+                    'user_id' => $user->id,
+                ]);
+        }
     }
 }
