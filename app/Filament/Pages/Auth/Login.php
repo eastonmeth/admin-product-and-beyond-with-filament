0a9b7b075a39da0ec681a061730c84330a50e531
Easton Meth <easton.meth@gmail.com>
2025-02-14 15:18:26 -0500

    Auto add login details

---
+++ app/Filament/Pages/Auth/Login.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Pages\Auth\Login as BasePage;
+
+class Login extends BasePage
+{
+    public function mount(): void
+    {
+        parent::mount();
+
+        $this->form->fill([
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'remember' => true,
+        ]);
+    }
+}
