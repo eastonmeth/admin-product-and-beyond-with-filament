3c3e40b954361914d09645ad2f38b4e25bc7704c
Easton Meth <easton.meth@gmail.com>
2025-02-19 15:06:26 -0500

    Add infolist

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -6,7 +6,11 @@ use App\Filament\Resources\PostResource\Pages;
 use App\Models\Post;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Components\TextEntry\TextEntrySize;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
 use Filament\Tables\Table;
 
@@ -84,6 +88,50 @@ class PostResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\TextEntry::make('title')
+                    ->hiddenLabel()
+                    ->size(TextEntrySize::Large)
+                    ->weight(FontWeight::SemiBold),
+                Infolists\Components\ImageEntry::make('image_url')
+                    ->height(300)
+                    ->hiddenLabel()
+                    ->columnSpanFull(),
+                Infolists\Components\TextEntry::make('description')
+                    ->hiddenLabel()
+                    ->columnSpanFull(),
+                Infolists\Components\Section::make('Details')
+                    ->columns(3)
+                    ->icon('heroicon-o-information-circle')
+                    ->schema([
+                        Infolists\Components\TextEntry::make('likes')
+                            ->badge()
+                            ->color('danger')
+                            ->icon('heroicon-c-heart')
+                            ->numeric(),
+                        Infolists\Components\TextEntry::make('created_at')
+                            ->dateTime(),
+                        Infolists\Components\TextEntry::make('updated_at')
+                            ->dateTime(),
+                    ]),
+                Infolists\Components\Section::make('User')
+                    ->columns(3)
+                    ->relationship('user')
+                    ->icon('heroicon-o-user')
+                    ->schema([
+                        Infolists\Components\ImageEntry::make('avatar_url')
+                            ->label('Avatar')
+                            ->size(50)
+                            ->circular(),
+                        Infolists\Components\TextEntry::make('name'),
+                        Infolists\Components\TextEntry::make('email'),
+                    ]),
             ]);
     }
 
