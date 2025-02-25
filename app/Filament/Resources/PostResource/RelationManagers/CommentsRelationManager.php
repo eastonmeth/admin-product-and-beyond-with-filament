f7495ae859a163e494c5c4cde4a78c0f9a5de751
Easton Meth <easton.meth@gmail.com>
2025-02-24 21:31:01 -0500

    Update comments relation manager

--- app/Filament/Resources/PostResource/RelationManagers/CommentsRelationManager.php
+++ app/Filament/Resources/PostResource/RelationManagers/CommentsRelationManager.php
@@ -4,8 +4,14 @@ namespace App\Filament\Resources\PostResource\RelationManagers;
 
 use App\Filament\Resources\UserResource;
 use App\Models\Comment;
-use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Actions\Action;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\TextEntry\TextEntrySize;
+use Filament\Infolists\Infolist;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Support\Enums\FontWeight;
 use Filament\Tables\Actions\BulkActionGroup;
@@ -13,6 +19,7 @@ use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -24,8 +31,9 @@ class CommentsRelationManager extends RelationManager
     public function form(Form $form): Form
     {
         return $form
+            ->columns(1)
             ->schema([
-                TextInput::make('content')
+                Textarea::make('content')
                     ->required(),
             ]);
     }
@@ -33,25 +41,29 @@ class CommentsRelationManager extends RelationManager
     public function table(Table $table): Table
     {
         return $table
-            ->recordTitleAttribute('content')
             ->columns([
                 ImageColumn::make('user.avatar_url')
                     ->label('User Avatar')
-                    ->circular()
-                    ->url(fn (Comment $record): string => UserResource::getUrl('view', ['record' => $record->user_id])),
+                    ->circular(),
                 TextColumn::make('user.name')
                     ->weight(FontWeight::Bold)
-                    ->sortable()
-                    ->url(fn (Comment $record): string => UserResource::getUrl('view', ['record' => $record->user_id])),
-                TextColumn::make('content'),
+                    ->sortable(),
+                TextColumn::make('content')
+                    ->limit(100),
             ])
             ->filters([
                 //
             ])
             ->headerActions([
-                CreateAction::make(),
+                CreateAction::make()
+                    ->mutateFormDataUsing(function (array $data): array {
+                        $data['user_id'] = auth()->id();
+
+                        return $data;
+                    }),
             ])
             ->actions([
+                ViewAction::make(),
                 EditAction::make(),
                 DeleteAction::make(),
             ])
@@ -60,5 +72,37 @@ class CommentsRelationManager extends RelationManager
                     DeleteBulkAction::make(),
                 ]),
             ]);
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->columns(1)
+            ->schema([
+                TextEntry::make('content')
+                    ->size(TextEntrySize::Large)
+                    ->hiddenLabel(),
+                Section::make('User')
+                    ->columns(3)
+                    ->relationship('user')
+                    ->icon('heroicon-o-user')
+                    ->headerActions([
+                        Action::make('view')
+                            ->url(fn (Comment $record): string => UserResource::getUrl('view', ['record' => $record->user_id])),
+                    ])
+                    ->schema([
+                        ImageEntry::make('avatar_url')
+                            ->label('Avatar')
+                            ->size(50)
+                            ->circular(),
+                        TextEntry::make('name'),
+                        TextEntry::make('email'),
+                    ]),
+            ]);
+    }
+
+    public function isReadOnly(): bool
+    {
+        return false;
     }
 }
