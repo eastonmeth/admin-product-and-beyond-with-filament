44d667554282fa55ad03a4d746b89b069effbbd2
Easton Meth <easton.meth@gmail.com>
2025-03-04 13:39:40 -0500

    Add status tabs to Posts list page

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\PostResource\Pages;
 
+use App\Enums\PostStatus;
 use App\Filament\Resources\PostResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListPosts extends ListRecords
 {
@@ -14,6 +17,19 @@ class ListPosts extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            Tab::make('All'),
+            ...collect(PostStatus::cases())
+                ->map(
+                    fn (PostStatus $status) => Tab::make($status->getLabel())
+                        ->icon($status->getIcon())
+                        ->query(fn (Builder $query) => $query->where('status', $status))
+                ),
         ];
     }
 }
